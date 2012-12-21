a2a5b410bae80c0597c8433d357024682848d468
Chris Mailloux <chrismayu@gmail.com>
2012-12-20 19:30:55 -0500

    5.3.4 Pretty RSpec  PG 211
    completed and test till 211

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,76 +3,13 @@ require 'spec_helper'
 describe "Static pages" do
 
   subject { page }
-  describe "Home page2" do
 
-     it "should have the h1 'Sample App'" do
-       visit '/static_pages/home'
-       page.should have_selector('h1', :text => 'Sample App')
-     end
-
-     it "should have the base title" do
-       visit '/static_pages/home'
-       page.should have_selector('title',
-                         :text => "Ruby on Rails Tutorial Sample App")
-     end
-
-     it "should not have a custom page title" do
-       visit '/static_pages/home'
-       page.should_not have_selector('title', :text => '| Home')
-     end
-   end
-   
-   
   describe "Home page" do
-    
-    it "should have the h1 'Sample App'" do
-         visit '/static_pages/home'
-         page.should have_selector('h1', :text => 'Sample App')
-       end
-
-       it "should have the base title" do
-         visit '/static_pages/home'
-         page.should have_selector('title',
-                           :text => "Ruby on Rails Tutorial Sample App")
-       end
-
-       it "should not have a custom page title" do
-         visit '/static_pages/home'
-         page.should_not have_selector('title', :text => '| Home')
-       end
-    
     before { visit root_path }
 
     it { should have_selector('h1',    text: 'Sample App') }
     it { should have_selector('title', text: full_title('')) }
     it { should_not have_selector 'title', text: '| Home' }
-
-    describe "for signed-in users" do
-      let(:user) { FactoryGirl.create(:user) }
-      before do
-        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
-        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
-        sign_in user
-        visit root_path
-      end
-
-      it "should render the user's feed" do
-        user.feed.each do |item|
-          page.should have_selector("li##{item.id}", text: item.content)
-        end
-      end
-
-      describe "follower/following counts" do
-        let(:other_user) { FactoryGirl.create(:user) }
-        before do
-          other_user.follow!(user)
-          visit root_path
-        end
-
-        it { should have_link("0 following", href: following_user_path(user)) }
-        it { should have_link("1 followers", href: followers_user_path(user)) }
-      end
-    end
   end
 
   describe "Help page" do
@@ -94,20 +31,5 @@ describe "Static pages" do
 
     it { should have_selector('h1',    text: 'Contact') }
     it { should have_selector('title', text: full_title('Contact')) }
-  end
-
-  it "should have the right links on the layout" do
-    visit root_path
-    click_link "About"
-    page.should have_selector 'title', text: full_title('About Us')
-    click_link "Help"
-    page.should have_selector 'title', text: full_title('Help')
-    click_link "Contact"
-    page.should have_selector 'title', text: full_title('Contact')
-    click_link "Home"
-    click_link "Sign up now!"
-    page.should have_selector 'title', text: full_title('Sign up')
-    click_link "sample app"
-    page.should have_selector 'h1', text: 'Sample App'
   end
 end
